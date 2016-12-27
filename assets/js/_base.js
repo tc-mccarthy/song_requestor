6c80be065a6321a3b9a9d4298fda87672c65ff60
TC McCarthy <tc.mccarthy1@gmail.com>
2016-12-27 02:10:12 -0500

    All API stuff is working

--- assets/js/_base.js
+++ assets/js/_base.js
@@ -1,5 +1,6 @@
 var app = {
     ele: {},
+    template: {},
     init: function() {
         var _this = this;
         $(_this.onReady.bind(_this));
@@ -11,6 +12,7 @@ var app = {
     onReady: function() {
         var _this = this;
         this.binds();
+        this.registerTemplates();
     },
 
     onLoad: function() {
@@ -27,6 +29,99 @@ var app = {
 
     binds: function() {
         var _this = this;
+
+        $("body").on("keyup", "#search", {context: _this}, _this.searchAutocomplete);
+        $("body").on("click", "a.play", {context: _this}, _this.startTrack);
+        $("body").on("click", "a.pause", {context: _this}, _this.stopTrack);
+    },
+
+    registerTemplates: function(){
+      var _this = this;
+      _this.registerTemplate($("#results_template"), "results");
+    },
+
+    registerTemplate: function(ele, key){
+      var _this = this;
+
+      if(ele.length > 0){
+        _this.template[key] = _.template(ele.html());
+      }
+    },
+
+    searchAutocomplete: function(e){
+      var term = $("#search").val(),
+          _this = e.data.context;
+
+      if(term.length > 5){
+        $("#results").html("");
+        _this.spotifyLookup(term, "track", function(data){
+          var items = _.map(data.tracks.items, function(item){
+            return {
+              track_name: item.name,
+              spotify_id: item.id,
+              artist: item.artists[0].name,
+              album: item.album.name,
+              album_image: item.album.images[1].url,
+              preview_track: item.preview_url
+            };
+          });
+
+          _.each(items, function(item){
+            $("#results").append(_this.template.results({item: item}));
+          });
+
+
+        });
+      }
+    },
+
+    spotifyLookup: function(term, type, cb){
+      var _this = this,
+          response;
+      $.getJSON("https://api.spotify.com/v1/search?type=track&q=" + term, function(success){
+        response = success;
+      }).done(function(){
+        cb(response);
+      });
+    },
+
+    startTrack: function(e){
+      e.preventDefault();
+
+      var ele = $(this),
+          _this = e.data.context,
+          audio = ele.next("audio");
+
+      ele.removeClass("play").addClass("pause");
+
+      $("audio").each(function(){
+        var ele = $(this);
+        _this.pause(ele);
+      });
+
+      _this.play(audio);
+    },
+
+    stopTrack: function(e){
+      e.preventDefault();
+
+      var ele = $(this),
+          _this = e.data.context,
+          audio = ele.next("audio");
+
+      ele.removeClass("pause").addClass("play");
+
+      _this.pause(audio);
+    },
+
+    pause: function(audio){
+      audio[0].pause();
+      audio.closest(".item").find(".fa.fa-pause-circle-o").removeClass("fa-pause-circle-o").addClass("fa-play-circle-o");
+    },
+
+    play: function(audio){
+      audio[0].play();
+      audio.closest(".item").find(".fa.fa-play-circle-o").removeClass("fa-play-circle-o").addClass("fa-pause-circle-o");
     }
 };
 
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<meta charset="utf-8"> <!-- set the character set -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1"> <!-- first step to a responsive site -->
+	<title>Bootstrap starter template</title>
+
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+	<!-- Your styles should be called here -->
+  <link rel="stylesheet" href="css/style.min.css" />
+
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+  </head>
+  <body>
+  	<div class="container"> <!-- start bootstrap container -->
+      <input id="search" type="text" placeholder="Enter the name of the song you wish to request"></div>
+      <div id="results" class="container"></div>
+  	</div>
+
+  	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+
+  	<!-- Your javascript should be called here -->
+    <script src="js/app.min.js"></script>
+    <script id="results_template" type="text/template">
+      <div class='item grid'>
+          <div class='col-1-3 col-tab-1-8 img'>
+              <a class='play' href="#">
+                <img src='<%= item.album_image %>' />
+                <i class='fa fa-play-circle-o'></i>
+              </a>
+              <audio src="<%= item.preview_track %>"></audio>
+          </div>
+          <div class='col-1-3 col-tab-1-2 track_info'>
+            <%= item.track_name %> - <%= item.artist %> - <%= item.album %>
+          </div>
+          <div class='col-1-3 col-tab-3-8 request'>
+            <i class='fa fa-plus'></i>
+          </div>
+      </div>
+    </script>
+  </body>
+</html>
